e6e2705dff655a4df9cbb60f10c6d21fbfc63f5f
Lee Mei Qi <2605039@sit.singaporetech.edu.sg>
2026-09-11 16:19:51 +0800

    Second Post

--- auditor.py
+++ auditor.py
@@ -1,8 +1,8 @@
 inventory = 0
-user_quit = 'false'
+user_input = ''
 
-user_input = ""
 while user_input != 'quit':
     user_input = input("Enter stock quantity: ")
-else:
-    print("User has quit the program")
+
+    if user_input == 'quit':
+        break
