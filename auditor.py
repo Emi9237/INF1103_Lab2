222615f9b13a8d4e93ec5ab177f18181e7ea34f9
Lee Mei Qi <2605039@sit.singaporetech.edu.sg>
2026-09-11 16:28:10 +0800

    Sixth Post

--- auditor.py
+++ auditor.py
@@ -1,5 +1,6 @@
 inventory = 0
 user_input = ''
+rejected = 0
 
 while user_input != 'quit':
     user_input = input("Enter stock quantity: ")
@@ -12,3 +13,4 @@ while user_input != 'quit':
         if inventory > 500:
             print("Inventory has exceeded 500 units")
             print("Total Units Processed ", inventory)
+            print("Number of Failed/Rejected Entries ", rejected)
